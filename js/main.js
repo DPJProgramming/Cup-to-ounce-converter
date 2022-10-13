77adfa696bc0660fff45310685779c0bf3c4d64d
David Jarvis <cheesyd64@hotmail.com>
2022-10-13 11:55:28 -0700

    Setup ts and js with onclick for convert button

---
+++ js/main.js
@@ -0,0 +1,3 @@
+window.onload = function onClick() {
+    GamepadButton.onclick = validate;
+};
---
+++ ts/main.ts
@@ -0,0 +1,7 @@
+window.onload = function onClick(){
+    document.getElementById("convert").onclick = main;
+}
+
+function main():void{
+
+}
